0135459837656419c49fec5c7521e87854ac08a9
ajijsk <ajijsk240@gmail.com>
2023-05-17 12:24:07 +0530

    odify

--- src/formSource.js
+++ src/formSource.js
@@ -1,77 +1,119 @@
 export const userInputs = [
-    {
-      id: 1,
-      label: "Username",
-      type: "text",
-      placeholder: "john_doe",
-    },
-    {
-      id: 2,
-      label: "Name and surname",
-      type: "text",
-      placeholder: "John Doe",
-    },
-    {
-      id: 3,
-      label: "Email",
-      type: "mail",
-      placeholder: "john_doe@gmail.com",
-    },
-    {
-      id: 4,
-      label: "Phone",
-      type: "text",
-      placeholder: "+1 234 567 89",
-    },
-    {
-      id: 5,
-      label: "Password",
-      type: "password",
-    },
-    {
-      id: 6,
-      label: "Address",
-      type: "text",
-      placeholder: "Elton St. 216 NewYork",
-    },
-    {
-      id: 7,
-      label: "Country",
-      type: "text",
-      placeholder: "USA",
-    },
-  ];
-  
-  export const productInputs = [
-    {
-      id: 1,
-      label: "Title",
-      type: "text",
-      placeholder: "Apple Macbook Pro",
-    },
-    {
-      id: 2,
-      label: "Description",
-      type: "text",
-      placeholder: "Description",
-    },
-    {
-      id: 3,
-      label: "Category",
-      type: "text",
-      placeholder: "Computers",
-    },
-    {
-      id: 4,
-      label: "Price",
-      type: "text",
-      placeholder: "100",
-    },
-    {
-      id: 5,
-      label: "Stock",
-      type: "text",
-      placeholder: "in stock",
-    },
-  ];
-  
+  {
+    id: "username",
+    label: "Username",
+    type: "text",
+    placeholder: "john_doe",
+  },
+
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "john_doe@gmail.com",
+  },
+  {
+    id: "phone",
+    label: "Phone",
+    type: "text",
+    placeholder: "+1 234 567 89",
+  },
+  {
+    id: "password",
+    label: "Password",
+    type: "password",
+  },
+
+  {
+    id: "country",
+    label: "Country",
+    type: "text",
+    placeholder: "USA",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "USA",
+  },
+];
+
+export const hotelInputs = [
+  {
+    id: "HotelName",
+    label: "Name",
+    type: "text",
+    placeholder: "My Hotel",
+  },
+  {
+    id: "type",
+    label: "Type",
+    type: "text",
+    placeholder: "My Hotel",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "West Bengal",
+  },
+
+  {
+    id: "address",
+    label: "Address",
+    type: "text",
+    placeholder: "Hotel Address",
+  },
+  {
+    id: "distance",
+    label: "Distance from City Centre",
+    type: "text",
+    placeholder: "500 m",
+  },
+  {
+    id: "title",
+    label: "Title",
+    type: "text",
+    placeholder: "The Best Hotel",
+  },
+
+  {
+    id: "description",
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: "price",
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+];
+
+export const roomInput =[
+  {
+    id: "title",
+    label: "Title",
+    type: "text",
+    placeholder: " 2 bed room",
+  },
+  {
+    id: "description",
+    label: "Description",
+    type: "text",
+    placeholder: "king size bed:1 bed room",
+  },
+  {
+    id: "maxPeople",
+    label: "Max-People",
+    type: Number,
+    placeholder: 2,
+  },
+  {
+    id: "price",
+    label: "Price",
+    type: Number,
+    placeholder: 100,
+  },
+]
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,63 @@
-import "./login.scss"
+import { AuthContext } from "../../context/AuthContext copy";
+import "./login.scss";
+import axios from "axios"
+import React, { useContext, useState, } from 'react';
+import { useNavigate} from "react-router-dom";
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+const Login =()=>{
+
+    const [credentials, setCredentials]=useState({
+        username:undefined,
+        password:undefined,
+    })
+     const {loading,error,dispatch}=useContext(AuthContext);
+
+const navigate=useNavigate()
+
+     const handleChange=(e)=>{
+       setCredentials(prev=>({...prev,[e.target.id]:e.target.value}))
+     };
+
+     const handleClick= async e =>{
+         e.preventDefault()
+         dispatch({type:"LOGIN_START"})
+       
+         try{
+             
+             const res= await axios.post('/auth/login',credentials);
+            //    console.log("res.data",res.data.details.email)
+            //    console.log("res.data.password",res.data.password)
+
+           
+             if(res.data.details.isAdmin){
+              dispatch({type:"LOGIN_SUCCESS",payload:res.data.details})
+
+              navigate("/")
+             }else{
+              dispatch({type:"LOGIN_FAILURE",payload:{message:"You are not allowed!"}})
+             }
+            
+         }catch(err){
+            // console.log("server error")
+            dispatch({type:"LOGIN_FAILURE",payload:err.response.data, status:"wrong user"})
+         }
+     }
+
+      
+ 
+    return (
+        <div className="login">
+            <div className="lContainer">
+                <input type="text" placeholder="username" id="username" onChange={handleChange}  className="lInput"></input>
+
+                <input type="password" placeholder="password" id="password" onChange={handleChange}  className="lInput"></input>
+                
+                <button disabled={loading} onClick={handleClick} className="lButton">Login</button>
+                {error && <span>{error.message}</span>}
+            </div>
+           
+        </div>
+    )
 }
 
-export default Login
+export default Login;
